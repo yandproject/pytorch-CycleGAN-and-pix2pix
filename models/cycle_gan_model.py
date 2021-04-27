48c9afaf548d26efe3bc2b5e2077a93a2f5564eb
lomidez <49808801+lomidez@users.noreply.github.com>
2021-04-27 12:13:54 +0300

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -4,6 +4,50 @@ from util.image_pool import ImagePool
 from .base_model import BaseModel
 from . import networks
 #my comment
+import numpy as np
+        
+cuda0 = torch.device('cuda:0')
+a = torch.ones([1, 3, 256, 256], dtype=torch.float, device=cuda0)
+h = a.shape[2]
+w = a.shape[3]
+for eye_h in range(int(h*2/10 ),int(h*4.5/10)):
+    for eye_left in range(int(w*2/10 ),int(w*4/10)):
+        a[0][0][eye_h][eye_left] = 2
+        a[0][1][eye_h][eye_left] = 2
+        a[0][2][eye_h][eye_left] = 2
+    for eye_right in range(int(w*6/10 ),int(w*8/10)):
+        a[0][0][eye_h][eye_right] = 2
+        a[0][1][eye_h][eye_right] = 2
+        a[0][2][eye_h][eye_right] = 2
+for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
+    for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
+        a[0][0][lip_h][lip_w] = 2
+        a[0][1][lip_h][lip_w] = 2
+        a[0][2][lip_h][lip_w] = 2
+weights_1 = a        
+cuda0 = torch.device('cuda:0')
+b = torch.ones([1, 1, 256, 256], dtype=torch.float, device=cuda0)
+h = b.shape[2]
+w = b.shape[3]
+for eye_h in range(int(h*2/10),int(h*4.5/10)):
+    for eye_left in range(int(w*2/10 ),int(w*4/10)):
+        b[0][0][eye_h][eye_left] = 2
+    for eye_right in range(int(w*6/10 ),int(w*8/10)):
+        b[0][0][eye_h][eye_right] = 2
+
+for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
+    for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
+        b[0][0][lip_h][lip_w] = 2
+                
+weights_0 = b
+        
+        
+pool1 = torch.nn.AvgPool2d(4)
+pool2 = torch.nn.AvgPool2d(2)
+pool3 = torch.nn.AvgPool2d(3,stride=1)
+weights_0 = pool1(weights_0)
+weights_0 = pool2(weights_0)
+weights_0 = pool3(weights_0)
 
 
 class CycleGANModel(BaseModel):
@@ -134,27 +178,27 @@ class CycleGANModel(BaseModel):
         # Real
         ####################################################################
                 
-        cuda0 = torch.device('cuda:0')
-        b = torch.ones([1, 1, 256, 256], dtype=torch.float, device=cuda0)
-        h = b.shape[2]
-        w = b.shape[3]
-        for eye_h in range(int(h*2/10),int(h*4.5/10)):
-            for eye_left in range(int(w*2/10 ),int(w*4/10)):
-                b[0][0][eye_h][eye_left] = 2
-            for eye_right in range(int(w*6/10 ),int(w*8/10)):
-                b[0][0][eye_h][eye_right] = 2
+#         cuda0 = torch.device('cuda:0')
+#         b = torch.ones([1, 1, 256, 256], dtype=torch.float, device=cuda0)
+#         h = b.shape[2]
+#         w = b.shape[3]
+#         for eye_h in range(int(h*2/10),int(h*4.5/10)):
+#             for eye_left in range(int(w*2/10 ),int(w*4/10)):
+#                 b[0][0][eye_h][eye_left] = 2
+#             for eye_right in range(int(w*6/10 ),int(w*8/10)):
+#                 b[0][0][eye_h][eye_right] = 2
 
-        for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
-            for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
-                b[0][0][lip_h][lip_w] = 2
+#         for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
+#             for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
+#                 b[0][0][lip_h][lip_w] = 2
           
-        weights_0 = b
-        pool1 = torch.nn.AvgPool2d(4)
-        pool2 = torch.nn.AvgPool2d(2)
-        pool3 = torch.nn.AvgPool2d(3,stride=1)
-        weights_0 = pool1(weights_0)
-        weights_0 = pool2(weights_0)
-        weights_0 = pool3(weights_0)
+#         weights_0 = b
+#         pool1 = torch.nn.AvgPool2d(4)
+#         pool2 = torch.nn.AvgPool2d(2)
+#         pool3 = torch.nn.AvgPool2d(3,stride=1)
+#         weights_0 = pool1(weights_0)
+#         weights_0 = pool2(weights_0)
+#         weights_0 = pool3(weights_0)
          
         
         pred_real = netD(real)
@@ -181,52 +225,52 @@ class CycleGANModel(BaseModel):
     def backward_G(self):
         """Calculate the loss for generators G_A and G_B"""
         #########################################
-        import numpy as np
+#         import numpy as np
         
-        cuda0 = torch.device('cuda:0')
-        a = torch.ones([1, 3, 256, 256], dtype=torch.float, device=cuda0)
-        h = a.shape[2]
-        w = a.shape[3]
-        for eye_h in range(int(h*2/10 ),int(h*4.5/10)):
-            for eye_left in range(int(w*2/10 ),int(w*4/10)):
-                a[0][0][eye_h][eye_left] = 2
-                a[0][1][eye_h][eye_left] = 2
-                a[0][2][eye_h][eye_left] = 2
-            for eye_right in range(int(w*6/10 ),int(w*8/10)):
-                a[0][0][eye_h][eye_right] = 2
-                a[0][1][eye_h][eye_right] = 2
-                a[0][2][eye_h][eye_right] = 2
+#         cuda0 = torch.device('cuda:0')
+#         a = torch.ones([1, 3, 256, 256], dtype=torch.float, device=cuda0)
+#         h = a.shape[2]
+#         w = a.shape[3]
+#         for eye_h in range(int(h*2/10 ),int(h*4.5/10)):
+#             for eye_left in range(int(w*2/10 ),int(w*4/10)):
+#                 a[0][0][eye_h][eye_left] = 2
+#                 a[0][1][eye_h][eye_left] = 2
+#                 a[0][2][eye_h][eye_left] = 2
+#             for eye_right in range(int(w*6/10 ),int(w*8/10)):
+#                 a[0][0][eye_h][eye_right] = 2
+#                 a[0][1][eye_h][eye_right] = 2
+#                 a[0][2][eye_h][eye_right] = 2
 
-        for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
-            for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
-                a[0][0][lip_h][lip_w] = 2
-                a[0][1][lip_h][lip_w] = 2
-                a[0][2][lip_h][lip_w] = 2
+#         for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
+#             for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
+#                 a[0][0][lip_h][lip_w] = 2
+#                 a[0][1][lip_h][lip_w] = 2
+#                 a[0][2][lip_h][lip_w] = 2
                 
-        weights_1 = a        
-        cuda0 = torch.device('cuda:0')
-        b = torch.ones([1, 1, 256, 256], dtype=torch.float, device=cuda0)
-        h = b.shape[2]
-        w = b.shape[3]
-        for eye_h in range(int(h*2/10),int(h*4.5/10)):
-            for eye_left in range(int(w*2/10 ),int(w*4/10)):
-                b[0][0][eye_h][eye_left] = 2
-            for eye_right in range(int(w*6/10 ),int(w*8/10)):
-                b[0][0][eye_h][eye_right] = 2
+#         weights_1 = a        
+#         cuda0 = torch.device('cuda:0')
+#         b = torch.ones([1, 1, 256, 256], dtype=torch.float, device=cuda0)
+#         h = b.shape[2]
+#         w = b.shape[3]
+#         for eye_h in range(int(h*2/10),int(h*4.5/10)):
+#             for eye_left in range(int(w*2/10 ),int(w*4/10)):
+#                 b[0][0][eye_h][eye_left] = 2
+#             for eye_right in range(int(w*6/10 ),int(w*8/10)):
+#                 b[0][0][eye_h][eye_right] = 2
 
-        for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
-            for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
-                b[0][0][lip_h][lip_w] = 2
+#         for lip_h in range(int(h*7/10 ),int(h*8.5/10)): 
+#             for lip_w in range(int(w*3.5/10 ),int(w*6.5/10)):
+#                 b[0][0][lip_h][lip_w] = 2
                 
-        weights_0 = b
+#         weights_0 = b
         
         
-        pool1 = torch.nn.AvgPool2d(4)
-        pool2 = torch.nn.AvgPool2d(2)
-        pool3 = torch.nn.AvgPool2d(3,stride=1)
-        weights_0 = pool1(weights_0)
-        weights_0 = pool2(weights_0)
-        weights_0 = pool3(weights_0)
+#         pool1 = torch.nn.AvgPool2d(4)
+#         pool2 = torch.nn.AvgPool2d(2)
+#         pool3 = torch.nn.AvgPool2d(3,stride=1)
+#         weights_0 = pool1(weights_0)
+#         weights_0 = pool2(weights_0)
+#         weights_0 = pool3(weights_0)
         
         #weights_1 = torch.ones([1, 3, 256, 256], dtype=torch.int, device=cuda0)
         #weights_1 = torch.ones(3, 256, 256) #create test weights all 1s
