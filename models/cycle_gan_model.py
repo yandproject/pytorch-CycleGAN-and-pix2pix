8c0854868078b6bc542a9e312a32043f5a996944
lomidez <49808801+lomidez@users.noreply.github.com>
2021-05-20 23:37:18 +0300

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -12,19 +12,19 @@ w = a.shape[3]
 #JACK
 for eye_h in range(int(h*7/20 ),int(h*9/20)):
     for eye_left in range(int(w*6/20),int(w*9/20)):
-        a[0][0][eye_h][eye_left] = 9
-        a[0][1][eye_h][eye_left] = 9
-        a[0][2][eye_h][eye_left] = 9
+        a[0][0][eye_h][eye_left] = 5
+        a[0][1][eye_h][eye_left] = 2
+        a[0][2][eye_h][eye_left] = 5
     for eye_right in range(int(w*11/20 ),int(w*14/20)):
-        a[0][0][eye_h][eye_right] = 9
-        a[0][1][eye_h][eye_right] = 9
-        a[0][2][eye_h][eye_right] = 9
+        a[0][0][eye_h][eye_right] = 5
+        a[0][1][eye_h][eye_right] = 2
+        a[0][2][eye_h][eye_right] = 5
 
 for lip_h in range(int(h*12/20 ),int(h*14/20)): 
     for lip_w in range(int(w*8/20 ),int(w*12/20)):
-        a[0][0][lip_h][lip_w] = 9
-        a[0][1][lip_h][lip_w] = 9
-        a[0][2][lip_h][lip_w] = 9
+        a[0][0][lip_h][lip_w] = 20
+        a[0][1][lip_h][lip_w] = 20
+        a[0][2][lip_h][lip_w] = 20
 #HICCUP
 # for eye_h in range(int(h*2/10 ),int(h*4.5/10)):
 #     for eye_left in range(int(w*2/10 ),int(w*4/10)):
@@ -50,13 +50,13 @@ w = b.shape[3]
 #JACK
 for eye_h in range(int(h*7/20 ),int(h*9/20)):
     for eye_left in range(int(w*6/20),int(w*9/20)):
-        b[0][0][eye_h][eye_left] = 9
+        b[0][0][eye_h][eye_left] = 5
     for eye_right in range(int(w*11/20),int(w*14/20)):
-        b[0][0][eye_h][eye_right] = 9
+        b[0][0][eye_h][eye_right] = 5
         
 for lip_h in range(int(h*12/20 ),int(h*14/20)): 
     for lip_w in range(int(w*8/20 ),int(w*12/20)):
-        b[0][0][lip_h][lip_w] = 9
+        b[0][0][lip_h][lip_w] = 20
         
 #HICCUP        
 # for eye_h in range(int(h*2/10),int(h*4.5/10)):
