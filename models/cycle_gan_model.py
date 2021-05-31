74285f5c395aeca4f1544a51fa2dd4087ea835bd
lomidez <49808801+lomidez@users.noreply.github.com>
2021-05-31 21:57:44 +0300

    Update cycle_gan_model.py

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -236,12 +236,12 @@ class CycleGANModel(BaseModel):
          
         
         pred_real = netD(real)
-        loss_D_real = (self.criterionGAN(pred_real, True)* weights_0)[weights_0 > 0].mean()
-        #loss_D_real = (self.criterionGAN(pred_real, True)).mean()
+        #loss_D_real = (self.criterionGAN(pred_real, True)* weights_0)[weights_0 > 0].mean()
+        loss_D_real = (self.criterionGAN(pred_real, True)).mean()
         # Fake
         pred_fake = netD(fake.detach())
-        loss_D_fake = (self.criterionGAN(pred_fake, False)* weights_0)[weights_0 > 0].mean()
-        #loss_D_fake = (self.criterionGAN(pred_fake, False)).mean()
+        #loss_D_fake = (self.criterionGAN(pred_fake, False)* weights_0)[weights_0 > 0].mean()
+        loss_D_fake = (self.criterionGAN(pred_fake, False)).mean()
         #########################################################################
         # Combined loss and calculate gradients
         loss_D = (loss_D_real + loss_D_fake) * 0.5
@@ -327,22 +327,22 @@ class CycleGANModel(BaseModel):
             self.loss_idt_B = 0
 
         # GAN loss D_A(G_A(A))
-        self.loss_G_A = (self.criterionGAN(self.netD_A(self.fake_B), True)* weights_0)[weights_0 > 0].mean()
-        #self.loss_G_A = (self.criterionGAN(self.netD_A(self.fake_B), True)).mean()
+        #self.loss_G_A = (self.criterionGAN(self.netD_A(self.fake_B), True)* weights_0)[weights_0 > 0].mean()
+        self.loss_G_A = (self.criterionGAN(self.netD_A(self.fake_B), True)).mean()
         # GAN loss D_B(G_B(B))
-        self.loss_G_B = (self.criterionGAN(self.netD_B(self.fake_A), True)* weights_0)[weights_0 > 0].mean()
-        #self.loss_G_B = (self.criterionGAN(self.netD_B(self.fake_A), True)).mean()
+        #self.loss_G_B = (self.criterionGAN(self.netD_B(self.fake_A), True)* weights_0)[weights_0 > 0].mean()
+        self.loss_G_B = (self.criterionGAN(self.netD_B(self.fake_A), True)).mean()
         #############################################
 #         # Forward cycle loss || G_B(G_A(A)) - A||
 #         self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
 #         # Backward cycle loss || G_A(G_B(B)) - B||
 #         self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
         # Forward cycle loss || G_B(G_A(A)) - A||
-        self.loss_cycle_A = (self.criterionCycle(self.rec_A, self.real_A)* weights_1)[weights_1 > 0].mean() * lambda_A 
-        #self.loss_cycle_A = (self.criterionCycle(self.rec_A, self.real_A)).mean() * lambda_A 
+        #self.loss_cycle_A = (self.criterionCycle(self.rec_A, self.real_A)* weights_1)[weights_1 > 0].mean() * lambda_A 
+        self.loss_cycle_A = (self.criterionCycle(self.rec_A, self.real_A)).mean() * lambda_A 
         # Backward cycle loss || G_A(G_B(B)) - B||
-        self.loss_cycle_B = (self.criterionCycle(self.rec_B, self.real_B) * weights_1)[weights_1 > 0].mean() * lambda_B
-        #self.loss_cycle_B = (self.criterionCycle(self.rec_B, self.real_B)).mean() * lambda_B
+        #self.loss_cycle_B = (self.criterionCycle(self.rec_B, self.real_B) * weights_1)[weights_1 > 0].mean() * lambda_B
+        self.loss_cycle_B = (self.criterionCycle(self.rec_B, self.real_B)).mean() * lambda_B
         ############################################
         # combined loss and calculate gradients
         self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
